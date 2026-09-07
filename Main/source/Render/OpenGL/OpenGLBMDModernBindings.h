4be381e6ac085cb2c4198c3b5900ad5170b0734c
zettoesports-glitch <zettoesports@gmail.com>
2026-09-07 05:22:01 -0700

    Add modern BMD sampler bindings

---
+++ Main/source/Render/OpenGL/OpenGLBMDModernBindings.h
@@ -0,0 +1,24 @@
+#pragma once
+
+class OpenGLSkeletonTexture;
+
+class OpenGLBMDModernBindings
+{
+public:
+    static const unsigned int MaterialTextureUnit = 0;
+    static const unsigned int SkeletonTextureUnit = 1;
+
+    OpenGLBMDModernBindings();
+
+    bool ConfigureProgram(unsigned int program);
+    bool Bind(unsigned int materialTexture,
+              const OpenGLSkeletonTexture& skeletonTexture) const;
+
+    unsigned int GetProgram() const { return m_Program; }
+    bool IsConfigured() const { return m_Program != 0; }
+
+private:
+    unsigned int m_Program;
+    int m_MaterialSamplerLocation;
+    int m_SkeletonSamplerLocation;
+};
