d3299520dae7731588a4f452b690ed1db6752568
zettoesports-glitch <zettoesports@gmail.com>
2026-09-07 05:21:46 -0700

    Implement OpenGL shared shader GlobalConstants buffer

---
+++ Main/source/Render/OpenGL/OpenGLShaderGlobalConstants.cpp
@@ -0,0 +1,84 @@
+#include "stdafx.h"
+#include "OpenGLShaderGlobalConstants.h"
+
+OpenGLShaderGlobalConstants::OpenGLShaderGlobalConstants()
+    : m_Buffer(0)
+{
+}
+
+OpenGLShaderGlobalConstants::~OpenGLShaderGlobalConstants()
+{
+    Destroy();
+}
+
+bool OpenGLShaderGlobalConstants::Initialize()
+{
+    if (m_Buffer != 0)
+        return true;
+
+    glGenBuffers(1, &m_Buffer);
+    if (m_Buffer == 0)
+        return false;
+
+    glBindBuffer(GL_UNIFORM_BUFFER, m_Buffer);
+    glBufferData(GL_UNIFORM_BUFFER,
+                 sizeof(Render::ShaderGlobalConstants),
+                 NULL,
+                 GL_DYNAMIC_DRAW);
+    glBindBufferBase(GL_UNIFORM_BUFFER,
+                     Render::ShaderGlobalConstantsBinding,
+                     m_Buffer);
+    glBindBuffer(GL_UNIFORM_BUFFER, 0);
+    return true;
+}
+
+bool OpenGLShaderGlobalConstants::ConfigureProgram(unsigned int program) const
+{
+    if (program == 0 || m_Buffer == 0)
+        return false;
+
+    const GLuint blockIndex = glGetUniformBlockIndex(program, "GlobalConstants");
+    if (blockIndex == GL_INVALID_INDEX)
+        return false;
+
+    glUniformBlockBinding(program,
+                          blockIndex,
+                          Render::ShaderGlobalConstantsBinding);
+    return true;
+}
+
+bool OpenGLShaderGlobalConstants::Upload(const Render::ShaderGlobalConstants& constants)
+{
+    if (!Initialize())
+        return false;
+
+    glBindBuffer(GL_UNIFORM_BUFFER, m_Buffer);
+    glBufferSubData(GL_UNIFORM_BUFFER,
+                    0,
+                    sizeof(Render::ShaderGlobalConstants),
+                    &constants);
+    glBindBufferBase(GL_UNIFORM_BUFFER,
+                     Render::ShaderGlobalConstantsBinding,
+                     m_Buffer);
+    glBindBuffer(GL_UNIFORM_BUFFER, 0);
+    return true;
+}
+
+void OpenGLShaderGlobalConstants::Bind() const
+{
+    if (m_Buffer != 0)
+    {
+        glBindBufferBase(GL_UNIFORM_BUFFER,
+                         Render::ShaderGlobalConstantsBinding,
+                         m_Buffer);
+    }
+}
+
+void OpenGLShaderGlobalConstants::Destroy()
+{
+    if (m_Buffer != 0)
+    {
+        glDeleteBuffers(1, &m_Buffer);
+        m_Buffer = 0;
+    }
+}
