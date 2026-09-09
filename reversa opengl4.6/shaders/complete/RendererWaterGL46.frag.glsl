ac0c968378dbf89e1ff01b570e983e453ec50305
zettoesports-glitch <zettoesports@gmail.com>
2026-09-08 20:14:38 -0700

    Add complete Water GL4.6 fragment shader

---
+++ reversa opengl4.6/shaders/complete/RendererWaterGL46.frag.glsl
@@ -0,0 +1,34 @@
+#version 460 core
+#define RENDERER_WATER_GL46 1
+
+in vec2 textureCoordinate;
+in vec4 vertexColor;
+in float viewDepth;
+uniform sampler2D textureSampler;
+uniform int alphaCutoff;
+layout(std140, binding = 0) uniform FrameData
+{
+    mat4 view;
+    mat4 projection;
+    mat4 viewProjection;
+    vec4 cameraPosition;
+    vec4 viewport;
+    vec4 timeFog;
+    vec4 tint;
+};
+layout(location = 0) out vec4 outputColor;
+void main()
+{
+    vec4 color = texture(textureSampler, textureCoordinate) * vertexColor;
+    if (alphaCutoff != 0 && color.a <= 0.25)
+    {
+        discard;
+    }
+    if (timeFog.w > 0.0)
+    {
+        float fogRange = max(timeFog.z - timeFog.y, 0.0001);
+        float fogFactor = clamp((viewDepth - timeFog.y) / fogRange, 0.0, 1.0);
+        color.rgb = mix(color.rgb, tint.rgb, fogFactor * clamp(timeFog.w, 0.0, 1.0));
+    }
+    outputColor = color;
+}
