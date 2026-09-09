c96e76776b311865a785eb0231f3ff63f4df6968
zettoesports-glitch <zettoesports@gmail.com>
2026-09-08 20:14:31 -0700

    Add complete Water GL4.6 vertex shader

---
+++ reversa opengl4.6/shaders/complete/RendererWaterGL46.vert.glsl
@@ -0,0 +1,27 @@
+#version 460 core
+#define RENDERER_WATER_GL46 1
+
+layout(location = 0) in vec3 inPosition;
+layout(location = 1) in vec2 inTextureCoordinate;
+layout(location = 2) in vec4 inColor;
+layout(std140, binding = 0) uniform FrameData
+{
+    mat4 view;
+    mat4 projection;
+    mat4 viewProjection;
+    vec4 cameraPosition;
+    vec4 viewport;
+    vec4 timeFog;
+    vec4 tint;
+};
+out vec2 textureCoordinate;
+out vec4 vertexColor;
+out float viewDepth;
+void main()
+{
+    vec4 viewPosition = view * vec4(inPosition, 1.0);
+    gl_Position = projection * viewPosition;
+    textureCoordinate = inTextureCoordinate;
+    vertexColor = inColor;
+    viewDepth = abs(viewPosition.z);
+}
