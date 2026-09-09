d5c5b86c4914f1d3af5d89c13841d6efc5bd96ef
zettoesports-glitch <zettoesports@gmail.com>
2026-09-08 20:14:53 -0700

    Add complete Cloth GL4.6 fragment shader

---
+++ reversa opengl4.6/shaders/complete/RendererClothGL46.frag.glsl
@@ -0,0 +1,38 @@
+#version 460 core
+#define RENDERER_CLOTH_GL46 1
+
+in vec2 textureCoordinate;
+in vec4 vertexColor;
+in float viewDepth;
+uniform sampler2D textureSampler;
+uniform int textureEnabled;
+uniform int alphaCutoffEnabled;
+uniform float alphaReference;
+layout(std140, binding = 0) uniform FrameData
+{
+    mat4 view;
+    mat4 projection;
+    mat4 viewProjection;
+    vec4 cameraPosition;
+    vec4 viewport;
+    vec4 timeFog;
+    vec4 tint;
+};
+layout(location = 0) out vec4 outputColor;
+void main()
+{
+    vec4 color = (textureEnabled != 0
+        ? texture(textureSampler, textureCoordinate)
+        : vec4(1.0)) * vertexColor;
+    if (alphaCutoffEnabled != 0 && color.a <= alphaReference)
+    {
+        discard;
+    }
+    if (timeFog.w > 0.0)
+    {
+        float fogRange = max(timeFog.z - timeFog.y, 0.0001);
+        float fogFactor = clamp((viewDepth - timeFog.y) / fogRange, 0.0, 1.0);
+        color.rgb = mix(color.rgb, tint.rgb, fogFactor * clamp(timeFog.w, 0.0, 1.0));
+    }
+    outputColor = color;
+}
